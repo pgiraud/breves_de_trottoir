66213559cd0aebdaba95199b6aef89fae60e4021
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-08-04 22:10:34 +0200

    No symbol

--- src/components/ActorItem.vue
+++ src/components/ActorItem.vue
@@ -3,12 +3,7 @@ defineProps(["actor", "symbol", "color"]);
 </script>
 
 <template>
-  <div
-    class="d-flex flex-column align-items-center border rounded px-2"
-    :style="{ 'background-color': `${color}cc` }"
-    style="width: 60px"
-  >
+  <div class="border rounded px-1 mx-1" :style="{ 'background-color': `${color}cc` }">
     {{ actor }}
-    <span v-html="symbol" class="symbol"> </span>
   </div>
 </template>
